62b73c0a54ac29bed101cea7cbe671c81bb92523
rohanjasons <rohan_jasons@hotmail.co.uk>
2020-02-21 19:04:05 +0000

    Update API
    Added comments

--- Api.Framework/Base/ApiBase.cs
+++ Api.Framework/Base/ApiBase.cs
@@ -3,30 +3,49 @@
 namespace Api.Framework
 {
     /// <summary>
-    /// 
+    /// This is the base class that all API tests will inherit from.
     /// </summary>
     public abstract class ApiBase
     {
-        protected RestClient RestClient;
-        protected RestRequest RestRequest;
-        protected IRestRequest IRestRequest;
-        protected IRestResponse RestResponse;
+        /// <summary>
+        ///  Client to translate RestRequests into HTTP requests and process response results
+        /// </summary>
+        public static RestClient RestClient;
 
         /// <summary>
-        /// 
+        /// Container for the data sent to the API
         /// </summary>
-        /// <param name="restClient"></param>
-        /// <param name="restRequest"></param>
-        /// <param name="restResponse"></param>
-        public ApiBase(RestClient restClient, RestRequest restRequest, IRestResponse restResponse)
+        public static RestRequest RestRequest;
+        
+        /// <summary>
+        /// Interface for the container for the data sent to the API
+        /// Contains the Endpoint, Headers, Body etc
+        /// </summary>
+        public static IRestRequest IRestRequest;
+
+        /// <summary>
+        /// Container for the data sent back from the API
+        /// Contains the response status code, response content etc
+        /// </summary>
+        public static IRestResponse RestResponse;
+
+        /// <summary>
+        /// Constructor used to initialise the API base
+        /// </summary>
+        /// <param name="restClient"> Client to translate RestRequests into HTTP requests and process response results </param>
+        /// <param name="restRequest"> Container for the data sent to the API </param>
+        /// <param name="iRestRequest"> Interface for the container for the data sent to the API </param>
+        /// <param name="restResponse"> Container for the data sent back from the API </param>
+        public ApiBase(RestClient restClient, RestRequest restRequest, IRestRequest iRestRequest, IRestResponse restResponse)
         {
             RestClient = restClient;
             RestRequest = restRequest;
             RestResponse = restResponse;
+            IRestRequest = iRestRequest;
         }
 
         /// <summary>
-        /// 
+        /// Empty constructor
         /// </summary>
         public ApiBase() { }
     }
